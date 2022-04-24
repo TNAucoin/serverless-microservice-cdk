9f31da6e82512676d93dc449c7ca13089dea9efc
TNAucoin <travisdevaucoin@gmail.com>
2022-04-23 22:33:48 -0500

    Update product.py
    Fixed routes to be more compliant with restful practices, added optional fields_include query param, to only return declared fields

--- src/product/lambda/api/api_v1/product.py
+++ src/product/lambda/api/api_v1/product.py
@@ -1,10 +1,12 @@
+from __future__ import annotations
+
 import logging
 import os
 import uuid
 from decimal import Decimal
+from typing import Optional, Dict
 
 import boto3
-from boto3.dynamodb.conditions import Key
 from fastapi import APIRouter
 from pydantic import BaseModel
 
@@ -32,21 +34,13 @@ class UpdateProduct(Product):
 ## Routes ##
 
 # Get products by product_category query param on /product
-@router.get('')
-async def get_products_by_category_endpoint(product_category: str):
-    return get_products_by_category(product_category)
-
-
-# Get product by product_id on product/
-@router.get('/{product_id}')
-async def get_products_by_id_endpoint(product_id: str):
-    return get_products_by_id(product_id)
-
-
-# Get all product
 @router.get('/')
-async def get_all_products_endpoint():
-    return get_all_products()
+async def get_products_by_category_endpoint(product_category: Optional[str] = None,
+                                            fields_include: Optional[str] = None):
+    if product_category is None:
+        return get_all_products(fields_include)
+    else:
+        return get_products_by_category(product_category, fields_include)
 
 
 # Create a product
@@ -55,35 +49,47 @@ async def create_product_endpoint(product: Product):
     return create_product(product)
 
 
+@router.put('/')
+async def update_product_endpoint(product: UpdateProduct):
+    return update_product(product)
+
+
+# Get product by product_id on product/
+@router.get('/{product_id}')
+async def get_products_by_id_endpoint(product_id: str):
+    return get_products_by_id(product_id)
+
+
 # Delete a product by product_id
 @router.delete('/{product_id}')
 async def delete_product_endpoint(product_id: str):
     pass
 
 
-@router.put('/')
-async def update_product_endpoint(product: UpdateProduct):
-    return update_product(product)
-
-
 ## Methods ##
-def get_products_by_id(product_id: str):
-    response = product_table.get_item(
-        Key={
-            "product_id": product_id
-        }
-    )
+def get_products_by_id(product_id: str, fields_include: str):
+    params = {
+        "Key": {"product_id": product_id}
+    }
+    # only return wanted fields if none specified return all
+    fields_include_query_modification(fields_include, params)
+    response = product_table.get_item(**params)
     if 'Item' in response:
         return response['Item']
     else:
         return {"message": "Product not found"}
 
 
-def get_products_by_category(product_category: str):
-    response = product_table.query(
-        IndexName='Category',
-        KeyConditionExpression=Key('category').eq(product_category)
-    )
+def get_products_by_category(product_category: str, fields_include: str):
+    params = {
+        "IndexName": "Category",
+        "KeyConditionExpression": "#category= :category",
+        "ExpressionAttributeValues": {":category": product_category},
+        "ExpressionAttributeNames": {"#category": "category"}
+    }
+    # only return wanted fields if none specified return all
+    fields_include_query_modification(fields_include, params)
+    response = product_table.query(**params)
     data = []
     if 'Items' in response:
         for item in response['Items']:
@@ -91,11 +97,17 @@ def get_products_by_category(product_category: str):
     return data
 
 
-def get_all_products():
-    response = product_table.scan()
+def get_all_products(fields_include: str):
+    params = {
+
+    }
+    # only return wanted fields if none specified return all
+    fields_include_query_modification(fields_include, params)
+    response = product_table.scan(**params)
     data = response['Items']
     while 'LastEvaluatedKey' in response:
-        response = product_table.scan(ExclusiveStartKey=response['LastEvaluatedKey'])
+        params['LastEvaluatedKey'] = response['LastEvaluatedKey']
+        response = product_table.scan(**params)
         data.extend(response['Items'])
     return data
 
@@ -137,3 +149,19 @@ def update_product(product: UpdateProduct):
         ReturnValues='UPDATED_NEW',
     )
     return response
+
+
+# Builds Projection Expression from string of product_attributes and adds it to the query params dict.
+def fields_include_query_modification(fields_include: str, query_params: Dict):
+    if fields_include is not None:
+        # catch for scan operations, query_params are {}, add empty dict
+        if "ExpressionAttributeNames" not in query_params:
+            query_params["ExpressionAttributeNames"] = {}
+
+        filter_attributes = fields_include.split(',')
+        expression_attributes = []
+        for attribute in filter_attributes:
+            exp_attr = f"#{attribute}"
+            query_params['ExpressionAttributeNames'][exp_attr] = attribute
+            expression_attributes.append(exp_attr)
+        query_params['ProjectionExpression'] = ",".join(expression_attributes)
